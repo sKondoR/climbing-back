17933df8a95d0377d7802e7186d7d3056a9e6af3
Sergey_Kondrashin <kondor85@mail.ru>
2024-09-26 02:02:24 +0300

    try to fix 41

--- src/app.service.ts
+++ src/app.service.ts
@@ -22,6 +22,7 @@ export class AppService {
     const electronPath = process.env.NODE_ENV?.includes('dev')
       ? electron
       : electron.substring(0, lastIndex);
+    console.log('Your nightmare Path: + ' + Nightmare);
     console.log('Your App Path: + ' + electronPath);
     const nightmare = Nightmare({
       // eslint-disable-next-line @typescript-eslint/no-require-imports
