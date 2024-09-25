abf0b3a9db21d5d8529d692c4ef83d607498fdf6
Sergey_Kondrashin <kondor85@mail.ru>
2024-09-25 23:30:14 +0300

    try to fix 24

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,8 +3,9 @@ import { Injectable } from '@nestjs/common';
 // import puppeteer from 'puppeteer-core';
 // eslint-disable-next-line @typescript-eslint/no-require-imports
 const puppeteer = require('puppeteer');
+import puppeteerCore from 'puppeteer-core';
 // eslint-disable-next-line @typescript-eslint/no-require-imports
-const chromium = require('@sparticuz/chromium-min');
+const chromium = require('@sparticuz/chromium');
 
 // const LOCAL_CHROME_EXECUTABLE =
 // 'C:/Users/Sergey_Kondrashin/.cache/puppeteer/chrome/win64-129.0.6668.58/chrome-win64/chrome.exe';
@@ -15,56 +16,49 @@ chromium.setGraphicsMode = false;
 @Injectable()
 export class AppService {
   async getClimberById(id: string): Promise<Array<string>> {
+    const options = process.env.AWS_REGION
+      ? {
+          ignoreHTTPSErrors: true,
+          args: chromium.args,
+          defaultViewport: chromium.defaultViewport,
+          executablePath: await chromium.executablePath(),
+          headless: chromium.headless,
+        }
+      : {
+          args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-gpu',
+            '--ignore-certificate-errors',
+            '--disable-extensions',
+          ],
+          executablePath: process.platform.includes('win')
+            ? 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe'
+            : process.platform === 'linux'
+              ? '/usr/bin/google-chrome'
+              : '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
+        };
+
+    // return [process.env.AWS_REGION, `${process.platform.includes('win')}`];
+    // const executablePath = process.env.NODE_ENV.includes('dev')
+    //   ? LOCAL_CHROME_EXECUTABLE
+    //   : await chromium?.executablePath();
+    // const args = process.env.NODE_ENV.includes('dev')
+    //   ? [
+    //       '--no-sandbox',
+    //       '--disable-setuid-sandbox',
+    //       '--disable-gpu',
+    //       '--ignore-certificate-errors',
+    //       '--disable-extensions',
+    //     ]
+    //   : chromium.args;
+
+    const browser = await puppeteerCore.launch(options);
+    const page = await browser.newPage();
+    let result = [];
+    // let doRequests = true;
+
     try {
-      const options = process.env.AWS_REGION
-        ? {
-            ignoreHTTPSErrors: true,
-            defaultViewport: {
-              width: 1920,
-              height: 1080,
-            },
-            args: [
-              ...chromium.args,
-              '--hide-scrollbars',
-              '--disable-web-security',
-            ],
-            executablePath: await chromium.executablePath(),
-            headless: chromium.headless,
-          }
-        : {
-            args: [
-              '--no-sandbox',
-              '--disable-setuid-sandbox',
-              '--disable-gpu',
-              '--ignore-certificate-errors',
-              '--disable-extensions',
-            ],
-            executablePath: process.platform.includes('win')
-              ? 'C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe'
-              : process.platform === 'linux'
-                ? '/usr/bin/google-chrome'
-                : '/Applications/Google Chrome.app/Contents/MacOS/Google Chrome',
-          };
-
-      // return [process.env.AWS_REGION, `${process.platform.includes('win')}`];
-      // const executablePath = process.env.NODE_ENV.includes('dev')
-      //   ? LOCAL_CHROME_EXECUTABLE
-      //   : await chromium?.executablePath();
-      // const args = process.env.NODE_ENV.includes('dev')
-      //   ? [
-      //       '--no-sandbox',
-      //       '--disable-setuid-sandbox',
-      //       '--disable-gpu',
-      //       '--ignore-certificate-errors',
-      //       '--disable-extensions',
-      //     ]
-      //   : chromium.args;
-
-      const browser = await puppeteer.launch(options);
-      const page = await browser.newPage();
-      let result = [];
-      // let doRequests = true;
-
       await page.goto(`https://www.allclimb.com/ru/climber/${id}`);
 
       // const button = await page.$$(BUTTON_SELECTOR);
@@ -99,7 +93,7 @@ export class AppService {
     } catch (error) {
       console.error('Error while scraping job listings:', error);
     } finally {
-      // await browser.close();
+      await browser.close();
     }
   }
 }
