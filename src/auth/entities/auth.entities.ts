99de8c8db6af072fde9b7b0ff2a7cbf062508809
Sergey_Kondrashin <kondor85@mail.ru>
2024-10-06 01:11:10 +0300

    validator

--- src/auth/entities/auth.entities.ts
+++ src/auth/entities/auth.entities.ts
@@ -1,7 +1,7 @@
-import { IsEmail, IsString } from 'class-validator';
+import { IsInt, IsString } from 'class-validator';
 
 export class AuthEntity {
-  @IsEmail()
+  @IsInt()
   allClimbId: number;
 
   @IsString()
