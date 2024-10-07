8e3e9b0a20bbc63be9b3bc7c51a56b57d9b95593
Sergey_Kondrashin <kondor85@mail.ru>
2024-10-07 09:00:57 +0300

    cors fix

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  const options = {
-    origin: '*',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-    credentials: true,
-  };
-  app.enableCors(options);
+  const app = await NestFactory.create(AppModule, { cors: true });
+  // const options = {
+  //   origin: '*',
+  //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+  //   preflightContinue: false,
+  //   optionsSuccessStatus: 204,
+  //   credentials: true,
+  // };
+  // app.enableCors(options);
   await app.listen(3000);
 }
 bootstrap();
