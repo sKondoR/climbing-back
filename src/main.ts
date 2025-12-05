dec2e40d7caca527a0b81cd9011036761ac49cc5
kondrse <kondor85@mail.ru>
2025-12-06 00:41:37 +0300

    try to fix cors

--- src/main.ts
+++ src/main.ts
@@ -16,27 +16,27 @@ async function bootstrap() {
 
   const app = await NestFactory.create(AppModule, { httpsOptions });
 
-  app.enableCors({
-    origin: [
-      'https://localhost',
-      'https://climbing-web.vercel.app',
-      'https://climbing-web.vercel.app/',
-    ],
-    credentials: true,
-    methods: ['GET', 'HEAD', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
-    allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
-    preflightContinue: false,
-    optionsSuccessStatus: 204,
-  });
+  // app.enableCors({
+  //   origin: [
+  //     'https://localhost',
+  //     'https://climbing-web.vercel.app',
+  //     'https://climbing-web.vercel.app/',
+  //   ],
+  //   credentials: true,
+  //   methods: ['GET', 'HEAD', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
+  //   allowedHeaders: ['Content-Type', 'Authorization', 'Accept'],
+  //   preflightContinue: false,
+  //   optionsSuccessStatus: 204,
+  // });
 
-    // const options = {
-    //   origin: '*',
-    //   methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    //   preflightContinue: false,
-    //   optionsSuccessStatus: 204,
-    //   credentials: true,
-    // };
-    // app.enableCors(options);
+    const options = {
+      origin: '*',
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+      preflightContinue: false,
+      optionsSuccessStatus: 204,
+      credentials: true,
+    };
+    app.enableCors(options);
 
   await app.listen(3000);
 }
