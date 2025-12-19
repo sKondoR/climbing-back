a99eb07670970d20abd9ec31c17d235c69be4229
kondrse <kondor85@mail.ru>
2025-12-19 09:55:32 +0300

    remove team hardcode endpoint

--- src/team/team.module.ts
+++ src/team/team.module.ts
@@ -4,11 +4,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { TeamMemberEntity } from './entities/team-member.entity';
 import { TeamService } from './team.service';
 import { TeamController } from './team.controller';
-import { TeamFullController } from './team-full.controller';
 
 @Module({
   imports: [TypeOrmModule.forFeature([TeamMemberEntity])],
-  controllers: [TeamController, TeamFullController],
+  controllers: [TeamController],
   providers: [TeamService],
 })
 export class TeamModule {}
